9f0eaf2cc316226ef6dc5595038d44ce8c4affee
maltaweel <m.altaweel@ucl.ac.uk>
2022-05-06 13:34:35 +0100

    commit

--- src/image/fragment_extrapolation.py
+++ src/image/fragment_extrapolation.py
@@ -4,12 +4,22 @@ Created on 5 May 2022
 @author: maltaweel
 '''
 import cv2
+import os
 import numpy as np
 from imutils import contours
 
+BLACK_THRESHOLD = 200
+THIN_THRESHOLD = 10
 
+#the path to the data folder
+pn=os.path.abspath(__file__)
+pn=pn.split("src")[0]  
+contours_directory=os.path.join(pn,'raw_contours')
+total_image=os.path.join(pn,'total_image')
 def fragExtract(images):
 
+    idx = 0
+    img=0
     for image in images:
         img_gray1 = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         ret, thresh1 = cv2.threshold(img_gray1, 150, 255, cv2.THRESH_BINARY)
@@ -19,12 +29,22 @@ def fragExtract(images):
         
         cv2.drawContours(image_copy2, contours2, -1, (0, 255, 0), 2, cv2.LINE_AA)
         cv2.imshow('SIMPLE Approximation contours', image_copy2)
-        cv2.waitKey(0)
         image_copy3 = image.copy()
+        
+        cv2.imwrite(os.path.join(total_image,'simple_'+str(img)+'.jpg'), image_copy3)
+        
+        img+=1
+        for cnt in contours2:
+            idx += 1
+            x, y, w, h = cv2.boundingRect(cnt)
+            roi = image[y:y + h, x:x + w]
+            if h < THIN_THRESHOLD or w < THIN_THRESHOLD:
+                continue
+            cv2.imwrite(os.path.join(contours_directory,str(idx) + '.png'), roi)
+            cv2.rectangle(image, (x, y), (x + w, y + h), (200, 0, 0), 2)
         
         cv2.imshow('CHAIN_APPROX_SIMPLE Point only', image_copy3)
         cv2.waitKey(0)
-        cv2.imwrite('contour_point_simple.jpg', image_copy3)
         cv2.destroyAllWindows()
         
   
